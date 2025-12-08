582745de66fea22697d324e3fd0971b935a27d5a
nitish <njindal@atlassian.com>
2025-12-08 18:34:31 +0530

    add calculator

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+/**
+ * abstract - not known at this moment, it will be defined in child class
+ *
+ */
+abstract public class Calculator {
+    abstract public Integer sum(int a, int b);
+    abstract public Integer sub(int a, int b);
+    abstract public Integer multi(int a, int b);
+    abstract public Integer div(int a, int b);
+}
---
+++ src/calculator/SimpleCalculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class SimpleCalculator extends Calculator {
+    public Integer sum(int a, int b) {
+        return 0;
+    }
+
+    public Integer sub(int a, int b) {
+        return 0;
+    }
+
+    public Integer multi(int a, int b) {
+        return 0;
+    }
+
+    public Integer div(int a, int b) {
+        return 0;
+    }
+}
